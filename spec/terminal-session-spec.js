804a5ca2f3b20ed1d11168a55c894af3d21c89e5
Justin Mecham <justin@mecham.me>
2018-01-11 20:24:49 -0500

    Fixed tests for new class names and code organization.

---
+++ spec/terminal-session-spec.js
@@ -0,0 +1,27 @@
+/** @babel */
+
+import TerminalSession from '../lib/terminal-session';
+import Xterm from 'xterm';
+
+describe('TerminalSession', () => {
+
+  let testSession;
+
+  beforeEach(() => {
+    testSession = new TerminalSession();
+  });
+
+  afterEach(() => {
+    testSession.destroy();
+  });
+
+  describe('xterm', () => {
+
+    it('instance is initialized', () => {
+      expect(testSession.xterm).not.toBe(null);
+      expect(testSession.xterm).toBeInstanceOf(Xterm);
+    });
+
+  });
+
+});
